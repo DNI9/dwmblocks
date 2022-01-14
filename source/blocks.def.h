8da3b62d5034ce83d2af6051c2395313d122816b
DNI9 <itsr9boi@gmail.com>
2022-01-15 01:16:21 +0530

    enable scripts

--- source/blocks.def.h
+++ source/blocks.def.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
 	// {"Icon", "Command", "update interval", "update signal"}
-	// {"", "~/.dwm/bin/song",						1,			0},
-	// {"", "~/.dwm/bin/speed",					1,			0},
-	// {"", "~/.dwm/bin/battery",				1,			0},
-	// {"", "~/.dwm/bin/brightness",			1,			0},
+	{"", "~/.dwm/bin/song",						1,			0},
+	{"", "~/.dwm/bin/speed",					1,			0},
+	{"", "~/.dwm/bin/battery",				1,			0},
+	{"", "~/.dwm/bin/brightness",			1,			0},
 	{"", "~/.dwm/bin/cpu",						1,			0},
 	{"", "~/.dwm/bin/memory",					1,			0},
-	// {"", "~/.dwm/bin/network",				1,			0},
+	{"", "~/.dwm/bin/network",				1,			0},
 	{"", "~/.dwm/bin/clock",					1,			0},
 	// {"", "/usr/share/dwm/scripts/packages",120,0},
 };
